414da0f6da7a1a779e1dac7a8b6bb204a18b4086
Widdy LOUIS <louisws@outlook.fr>
2022-08-14 13:31:17 +0200

    setting up the item wrapper that will contain the club list

--- custom_select_w_search_filter-js/index.js
+++ custom_select_w_search_filter-js/index.js
@@ -41,10 +41,46 @@ function createCustomDrompdown(dropdown) {
   dropdown.insertAdjacentElement('afterend', customDropdown);
 
   // créer un élément pour l'option sélectionnée
-  // ajouter une classe à cet élément, le texte de la première option dans le champ de sélection et l'ajouter à la liste déroulante personnalisée
+  // ajouter une classe à cet élément, le texte de la première option dans le champ de sélection et l'ajouter à la liste déroulante personnalisée  
   const selected = document.createElement('div');
   selected.className = 'dropdown__selected';
   selected.textContent = optionsArr[0].textContent;
-
   customDropdown.appendChild(selected);
+
+  // créer un élément pour le menu déroulant, y ajouter une classe et l'ajouter à la liste déroulante personnalisée ->  (va contenir un input et un wrapper d'items(dropdown__menu_items) > (dropdown__menu_item))
+  // ajouter un événement de clic à l'élément sélectionné pour basculer le menu déroulant
+  const menu = document.createElement('div');
+  menu.classList.add('dropdown__menu');
+  customDropdown.appendChild(menu);
+  selected.addEventListener('click', () => console.log('oui'));
+  // toggleDropdown.bind(menu)
+
+  // créer un élément d'entrée de recherche
+  // ajouter une class, un type et placeholder à cet élément et l'ajouter à l'élément de menu
+
+  const search = document.createElement('input');
+  search.placeholder = 'search...';
+  search.type = 'text';
+  search.className = 'dropdown__menu_search';
+  menu.appendChild(search);
+
+  // créer un élément wrapper(<div class="dropdown__menu_items"></div>) pour les éléments de menu, y ajouter une classe ajouter à l'élément de menu (va contenir les items <div class="dropdown__menu_item" data-value="">Sélectioner un club</div>)
+  const menuItemsWrapper = document.createElement('div');
+  menuItemsWrapper.className = 'dropdown__menu_items';
+  menu.appendChild(menuItemsWrapper);
+
+  // parcourir toutes les options et créer une option personnalisée pour chaque option et l'ajouter à l'élément wrapper des éléments
+  optionsArr.forEach( option => {
+    const item = document.createElement('div');
+    item.className = 'dropdown__menu_item';
+    item.dataset.value = option.value;
+    item.textContent = option.textContent;
+    menuItemsWrapper.appendChild(item);
+
+    item.addEventListener('click', () => console.log('oui'));
+    //setSelected.bind(item, selected, dropdown, menu)
+  });
+
+  // ajouter la class sélectionnée à la première option personnalisée
+  menuItemsWrapper.querySelector('div').className = 'selected';
 }
