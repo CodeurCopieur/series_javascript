69e828b8e4f749cf3d867d84a36c50cdc72c7f74
Widdy LOUIS <louisws@outlook.fr>
2022-08-22 23:21:11 +0200

    event scroll

--- scroll_spy_w_side_navigation-js/index.js
+++ scroll_spy_w_side_navigation-js/index.js
@@ -3,7 +3,7 @@ const links = [...document.querySelectorAll('nav a')];
 
 window.addEventListener('load', () => {
   // tableau d'offset de chaque section
-  const data = sections.map( (section) => section.offsetTop )
+  const data = sections.map( (section) => section.offsetTop );
   console.log(data);
 
   links.forEach( (link, i) => {
@@ -15,5 +15,41 @@ window.addEventListener('load', () => {
         behavior: "smooth"
       })
     })
-  })
+  });
+
+  window.addEventListener('scroll', handleScroll);
+
+  let saveIndex;
+  function handleScroll() {
+    const trigger = window.scrollY + (window.innerHeight/2);
+
+   for (const i of data) {
+    const index = data.indexOf(i);
+
+    if( trigger >= data[index] && trigger < data[index+1]) {
+      if(index !== saveIndex) {
+        saveIndex = index;
+        addClassAndClear(index)
+        break;
+      }
+    }
+
+    if( index === data.length - 1 && trigger >= data[index]) {
+      if(index !== saveIndex) {
+        saveIndex = index;
+        addClassAndClear(index)
+      }
+    }
+   }
+  }
+
+  function addClassAndClear(index) {
+    const elToClean = links.find( link => link.className.includes('marked'));
+
+    if (elToClean) {
+      elToClean.classList.remove('marked');
+    }
+
+    links[index].classList.add('marked');
+  }
 })
