3ea84d1fb1a619b620d18c1334801137382e80b5
Widdy LOUIS <louisws@outlook.fr>
2022-10-29 22:19:31 +0200

    heritages

--- les_classes/lesClasses.js
+++ les_classes/lesClasses.js
@@ -10,23 +10,55 @@ function moyenne(arrayNotes) {
 
 class Student {
   ecole = 'Voillaume'
+  _notes = []
+  #secret = 'variable pas exposés'
 
   constructor (firstname, lastname) {
     this.firstname = firstname,
     this.lastname = lastname
   }
 
-  setNotes(notes) {
-    this.notes = notes
+  // Setteur
+  set notes(v) {
+
+    if(Array.isArray(v))
+      this._notes = v
+  }
+  // setNotes(notes) {
+  //   this.notes = notes
+  // }
+
+  // getteur
+  get name() {
+    return this.firstname+' ' + this.lastname
   }
 
   canPass() {
-    return moyenne(this.notes) >= 10
+    return moyenne(this._notes) >= Student.moyenne
+  }
+
+  static moyenne = 10
+  
+}
+
+
+class SuperStudent extends Student {
+
+  constructor (firstname, lastname, notes) {
+    super(firstname, lastname)
+    this._notes = notes
+  }
+
+  get name() {
+    return 'Salut '+ super.name
+  }
+
+  canPass() {
+    return super.canPass()
   }
 }
 
-const wid = new Student('Wid', 'Lou')
+const wid = new SuperStudent('Wid', 'Lou', [12, 10, 9])
 const domi = new Student('Domi', 'Lou')
-wid.setNotes([10, 10, 8])
-domi.setNotes([15, 13, 17])
-console.log(wid.canPass(), domi.canPass());
+domi.notes = [15, 13, 17]
+console.log(wid._notes, wid.canPass());
