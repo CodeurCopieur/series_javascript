76433839d28cfbcbe06da81aeadaa20ee856e4c4
Widdy LOUIS <louisws@outlook.fr>
2022-11-03 22:48:57 +0100

    end

--- pratiquer_class-js/geometrie.js
+++ pratiquer_class-js/geometrie.js
@@ -11,6 +11,16 @@ class Rectangle {
   get isValid() {
     return this.w > 0 && this.h > 0
   }
+
+  isBiggerThan(shape) {
+    return this.perimetre > shape.perimetre
+  }
+}
+
+class Square extends Rectangle{
+  constructor(w){
+    super(w, w)
+  }
 }
 
 const r = new Rectangle(10, 20);
@@ -19,3 +29,6 @@ console.log(r.isValid);
 const r2 = new Rectangle(-10, 20);
 console.log(r2.perimetre);
 console.log(r2.isValid);
+const r3 = new Square(20);
+console.log(r3.perimetre);
+console.log(r.isBiggerThan(r3));
