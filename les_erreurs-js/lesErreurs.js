9867d0d7532ffaf207f178d567aa0d81675e469f
Widdy LOUIS <louisws@outlook.fr>
2022-11-16 23:52:18 +0100

    Create lesErreurs.js

---
+++ les_erreurs-js/lesErreurs.js
@@ -0,0 +1,39 @@
+class Rectangle {
+  constructor(w, h) {
+
+    if(w <= 0 || h <=0) {
+      throw new Error('Forme avecdes dimension négatives')
+    }
+
+    this.w = w,
+    this.h = h
+  }
+
+  get perimetre() {
+    return (this.w + this.h) * 2
+  }
+
+  get isValid() {
+    return this.w > 0 && this.h > 0
+  }
+
+  isBiggerThan(shape) {
+    return this.perimetre > shape.perimetre
+  }
+}
+
+class Square extends Rectangle{
+  constructor(w){
+    super(w, w)
+  }
+}
+
+const r = new Rectangle(10, 20);
+console.log(r.perimetre);
+console.log(r.isValid);
+const r2 = new Rectangle(-10, 20);
+console.log(r2.perimetre);
+console.log(r2.isValid);
+const r3 = new Square(20);
+console.log(r3.perimetre);
+console.log(r.isBiggerThan(r3));
