2a3b16ce8d2de704aaa9bd095a3d06787a8fa9a8
Widdy LOUIS <louisws@outlook.fr>
2022-12-06 00:01:00 +0100

    AbortController()

--- appel_http_fetch-js/index.js
+++ appel_http_fetch-js/index.js
@@ -1,10 +1,16 @@
 
-
+// Annuler une rêquete http quand on a déjà une reponse
+const a = new AbortController()
 
 Promise.race([
-  fetch('https://jsonplaceholder.typicode.com/posts/?_limit=5&_delay=2000'),
-  fetch('https://jsonplaceholder.typicode.com/users/?_limit=3')
-]).then( r => r.json()).then( console.log)
+  fetch('https://jsonplaceholder.typicode.com/posts/?_limit=5&_delay=2000', 
+  {signal: a.signal}),
+  fetch('https://jsonplaceholder.typicode.com/users/?_limit=3', 
+  {signal: a.signal})
+]).then( r => r.json()).then( body => {
+  a.abort()
+  console.log(body);
+})
 //   
 /*
     Promise.race([
