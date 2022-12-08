c8666f9c74fc22896251a1611e1afe6d642aa443
Widdy LOUIS <louisws@outlook.fr>
2022-12-08 19:08:37 +0100

    first commit

--- modules-js/array.js
+++ modules-js/array.js
@@ -1,3 +1,8 @@
-export const sum = (items) => items.reduce((acc, item) => acc + item, 0)
+import {bonjour, default as oui} from './hello.js'; // import
 
-export const hello = () => console.log('hello');
+export const sum = (items) => items.reduce((acc, item) => acc + item, 0)
+export default () => console.log('Test (hello)');
+export const hello = () => console.log('hello (array)');
+
+oui()
+export const testBonjour = bonjour
---
+++ modules-js/hello.js
@@ -0,0 +1,5 @@
+export default function() {
+  console.log('export default (hello) :)');
+}
+
+export const bonjour = () => console.log('bonjour  (hello)');
