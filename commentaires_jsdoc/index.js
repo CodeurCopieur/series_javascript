ec20f65060d7d969444644a1c59fd286de523f7f
Widdy LOUIS <louisws@outlook.fr>
2022-12-10 15:22:18 +0100

    end

--- commentaires_jsdoc/index.js
+++ commentaires_jsdoc/index.js
@@ -1,3 +1,4 @@
+// survoler avec la souris le nom de la function ou la variable
 
 /**
  * Permet de savoir si l'utilisateur peut conduire
@@ -14,10 +15,62 @@ function canDrive(age, country) {
   }
   return false
 }
-
+/**
+ * @param {number} age
+ * @returns {boolean}
+ */
 function isMajeur(age) {
   return age >= 18
 }
+// Array<string>
+// string[]
+// Promise<{id: number, title: string, body: string}[]>
+/**
+ *@returns {{id: number, title: string, body: string}}
+ */
+function fetch() {
 
-const a = canDrive(18, 'FR') // survoler avec la souris le nom de la function
-console.log(a);
+}
+
+
+
+/**
+ * @typedef {Object} Post
+ * @property {number} id
+ * @property {string} title titre de l'article
+ * @property {string} body
+ */
+
+/**
+ *@return {Promise<Post[]>}
+ */
+async function fetchPosts() {
+  
+}
+
+
+const a = canDrive(18, 'FR') 
+const b = fetch()
+const c = fetchPosts().then(posts => {
+  const post = posts[0]
+  // saisir 'post.' le detail des propriétés visible  
+})
+
+/**
+ *
+ *
+ * @class A
+ */
+class A {
+  constructor(firstname) {
+    this.firstname = firstname
+  }
+}
+
+const classA = new A();
+classA.firstname
+
+/**
+ * @type {string[]}
+ */
+const type = []
