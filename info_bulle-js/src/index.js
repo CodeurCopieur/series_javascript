c27df6b98c811e8568aa752c47fe0d067421f2f0
Widdy LOUIS <louisws@outlook.fr>
2022-12-27 16:29:52 +0100

    Create index.js

---
+++ info_bulle-js/src/index.js
@@ -0,0 +1,79 @@
+class Tooltip {
+  /**
+   * Applique le système de bulle d'infos sur les éléments
+   * @param {string} selector 
+   */
+  static bind(selector) {
+    document.querySelectorAll(selector).forEach( elt => new Tooltip(elt))
+  }
+  /**
+   * @param {HTMLElement} elt 
+   */
+  constructor(elt) {
+    this.elt = elt
+    this.title = elt.getAttribute('title')
+    this.tooltip = null
+    this.elt.addEventListener('mouseover', this.mouseOver.bind(this))
+    this.elt.addEventListener('mouseout', this.mouseOut.bind(this))
+    // this est object qui va contenir elt, title, tootip
+  }
+
+  mouseOver() {
+    let tooltip = this.createTooltip()
+    let w = this.tooltip.offsetWidth
+    let h = this.tooltip.offsetHeight
+    let l = this.elt.offsetWidth / 2 - w / 2 + this.elt.getBoundingClientRect().left + document.documentElement.scrollLeft
+    let t = this.elt.getBoundingClientRect().top - h - 15 + document.documentElement.scrollTop
+    tooltip.style.left = l+"px"
+    tooltip.style.top = t+"px"
+    tooltip.classList.add('visible')
+    // debugger 
+    // log this
+  }
+
+  mouseOut() {
+    if (this.tooltip !== null) {
+      
+      this.tooltip.classList.remove('visible')
+      this.tooltip.addEventListener('transitionend', ()=> {
+        document.body.removeChild(this.tooltip)
+        this.tooltip = null
+      })
+      
+    }
+  }
+
+  /**
+   * Créer et injecte la div dans le DOM
+   * @returns {HTMLElement}
+   */
+  createTooltip() {
+    if(this.tooltip === null){
+      let tooltip = document.createElement('div')
+      tooltip.innerHTML = this.title
+      tooltip.classList.add('tooltip')
+      document.body.appendChild(tooltip)
+      this.tooltip = tooltip
+    }
+    return this.tooltip
+  }
+}
+
+
+/*
+On sélectionne les éléments avec un attribut particulier (a[title])
+Lorsque l'on survole un élément {
+  On crée une <diV> dans le <body>
+  On remplit la <div> avec le texte correspondant au titre
+  On positionne la bulle au dessus de l'élément
+  On ajoute une classe, pour animer l'apparition
+}
+Lorsque l'on quitte le survole {
+  On retire une classe, pour animer la disparition
+  Lorsque l'animation se termine {
+    On supprime la <div> du <body>
+  }
+}
+*/
+
+Tooltip.bind('a[title]')
