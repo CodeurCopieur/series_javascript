d496bb85276d8e4920ef4c06e5d8f9ea4bb3933d
Widdy LOUIS <louisws@outlook.fr>
2022-12-29 22:14:27 +0100

    Test d'erreur

--- unit-testing_js/utils.test.js
+++ unit-testing_js/utils.test.js
@@ -32,3 +32,13 @@ test('getAllIds() devrait gérer le mauvais format d\'élément', ()=> {
   // Assert
   expect(res).toEqual([0, 1, null, 3, 4])
 })
+
+test('getAllIds() devrait lancer une erreur s\'il ne reçoit pas un tableau', ()=> {
+
+  // Arrange
+  const items = 'not-an-array'
+  // Act
+  const resMethod = ()=> getAllIds(items)
+  // Assert
+  expect(resMethod).toThrowError('items.map is not a function')
+})
