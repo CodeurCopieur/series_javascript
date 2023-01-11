17ffb305e18da774e75f0ecb6bf8833fa89d03c9
Widdy LOUIS <louisws@outlook.fr>
2023-01-11 20:53:43 +0100

    import TodoList

--- pratiquer_todo-list/src/index.js
+++ pratiquer_todo-list/src/index.js
@@ -1,11 +1,19 @@
+import { TodoList } from "./components/TodoList.js";
 import { fetchJSON } from './functions/api.js';
 import { createElt } from './functions/dom.js';
 
 
 try {
-  const todos = await fetchJSON('https://jsonplceholder.typicode.com/todos?_limit=5')
-  console.log(todos)
+
+  const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  // console.log(todos)
+  const list = new TodoList(todos)
+  list.appendTo(document.querySelector('#todolist'))
+
 } catch (error) {
+  /**
+   * Afficher une alerte si il ya dysfonctionement de l'API
+   */
   const div = createElt('div', {class: 'alert alert-danger', role: 'alert'}, 'Impossible de charger les éléments')
   document.body.prepend(div)
 }
