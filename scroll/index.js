46b350ea0836cb66bd93d06b8ec968d5789684ec
Widdy LOUIS <louisws@outlook.fr>
2023-02-12 11:16:56 +0100

    add

---
+++ scroll/index.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container');
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    console.log(scrollTop, clientHeight, scrollHeight );
+
+
+    if( scrollTop + clientHeight === scrollHeight) {
+        console.log("touché");
+        addArticles(10);
+    }
+});
+
+
+const addArticles = i => {
+    for (let index = 0; index < i ; index++) {
+        
+        const item = `
+            <div class="item"></div>
+        `;
+
+        container.innerHTML+=item;
+        
+    }
+}
