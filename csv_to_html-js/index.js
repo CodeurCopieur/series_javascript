7294fa07c8f66074ecca7759ab77f38b10860849
Widdy LOUIS <louisws@outlook.fr>
2023-03-15 01:13:14 +0100

    Lecteur de fichiers CSV

---
+++ csv_to_html-js/index.js
@@ -0,0 +1,33 @@
+// lecteur de fichiers + éléments html
+var reader = new FileReader(),
+    picker = document.getElementById('picker'),
+    table = document.getElementById('table')
+
+// lire csv sur le choix du fichier
+picker.onchange = () => reader.readAsText(picker.files[0]);
+
+// lire le fichier csv et générer du html
+reader.onloadend = () => {
+  // fichier csv entier
+  let csv = reader.result;
+
+  // efface le tableau html
+  table.innerHTML="";
+
+  // divisé en lignes
+  let rows = csv.split("\r\n");
+
+  // parcourir les lignes + diviser les colonnes
+  for (let row of rows) {
+    let cols = row.match(/(?:\"([^\"]*(?:\"\"[^\"]*)*)\")|([^\",]+)/g);
+    console.log(cols);
+    if (cols != null) {
+      let tr = table.insertRow();
+      for (let col of cols) {
+        let td = tr.insertCell();
+        td.innerHTML = col.replace(/(^"|"$)/g, "")
+      }
+    }
+  }
+}
+  
