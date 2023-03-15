e380e384bc2ebc84ab394caa8036b73396d3a524
Widdy LOUIS <louisws@outlook.fr>
2023-03-15 19:30:20 +0100

    AJAX Fetch CSV

--- csv_to_html-js/index.js
+++ csv_to_html-js/index.js
@@ -31,5 +31,33 @@
       }
     }
   }
+
+
+    // éléments html
+    var table2 = document.getElementById('table2')
+
+    // fetch csv
+    fetch('./0-dummy.csv')
+    .then( res => res.text())
+    .then( csv => {
+      console.log(csv);
+        // efface le tableau html
+      table2.innerHTML="";
+
+      // divisé en lignes
+      let rows = csv.split("\r\n");
+
+        // parcourir les lignes + diviser les colonnes
+        for (let row of rows) {
+        let cols = row.match(/(?:\"([^\"]*(?:\"\"[^\"]*)*)\")|([^\",]+)/g);
+          if (cols != null) {
+            let tr = table2.insertRow();
+            for (let col of cols) {
+              let td = tr.insertCell();
+              td.innerHTML = col.replace(/(^"|"$)/g, "")
+            }
+          }
+        }
+    })
 }());
   
