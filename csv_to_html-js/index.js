681449bccb8785fe113d16b197bb9cb60faa8ab3
Widdy LOUIS <louisws@outlook.fr>
2023-03-15 19:14:36 +0100

    Update index.js

--- csv_to_html-js/index.js
+++ csv_to_html-js/index.js
@@ -1,13 +1,15 @@
-// lecteur de fichiers + éléments html
-var reader = new FileReader(),
-    picker = document.getElementById('picker'),
-    table = document.getElementById('table')
+(function() {
+  // lecteur de fichiers + éléments html
+  var reader = new FileReader(),
+  picker = document.getElementById('picker'),
+  table = document.getElementById('table')
 
-// lire csv sur le choix du fichier
-picker.onchange = () => reader.readAsText(picker.files[0]);
+  // lire csv sur le choix du fichier
+  
+  picker.onchange = () => reader.readAsText(picker.files[0]);
 
-// lire le fichier csv et générer du html
-reader.onloadend = () => {
+  // lire le fichier csv et générer du html
+  reader.onloadend = () => {
   // fichier csv entier
   let csv = reader.result;
 
@@ -17,17 +19,17 @@ reader.onloadend = () => {
   // divisé en lignes
   let rows = csv.split("\r\n");
 
-  // parcourir les lignes + diviser les colonnes
-  for (let row of rows) {
+    // parcourir les lignes + diviser les colonnes
+    for (let row of rows) {
     let cols = row.match(/(?:\"([^\"]*(?:\"\"[^\"]*)*)\")|([^\",]+)/g);
-    console.log(cols);
-    if (cols != null) {
-      let tr = table.insertRow();
-      for (let col of cols) {
-        let td = tr.insertCell();
-        td.innerHTML = col.replace(/(^"|"$)/g, "")
+      if (cols != null) {
+        let tr = table.insertRow();
+        for (let col of cols) {
+          let td = tr.insertCell();
+          td.innerHTML = col.replace(/(^"|"$)/g, "")
+        }
       }
     }
   }
-}
+}());
   
