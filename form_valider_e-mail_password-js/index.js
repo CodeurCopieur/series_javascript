5889fcf87933b359dc47347d421d7708a9bc4316
Widdy LOUIS <louisws@outlook.fr>
2023-03-23 00:39:34 +0100

    checkEmail

--- form_valider_e-mail_password-js/index.js
+++ form_valider_e-mail_password-js/index.js
@@ -5,7 +5,6 @@ var Shaker = (() => {
   //Methode public car nous pouvons l'appeler de l'extérieur
   //Structure appeler le module parttern
   self.init = () => {
-    console.log('test');
 
     const form = document.querySelector('form'),
           emailField = form.querySelector('.email-field'),
@@ -13,7 +12,25 @@ var Shaker = (() => {
           passField = form.querySelector('.create-password'),
           passInput = passField.querySelector('.password'),
           cPassField = form.querySelector('.confirm-password'),
-          cPassInput = cPassField.querySelector('.cPassword')
+          cPassInput = cPassField.querySelector('.cPassword');
+    
+    // E-mail de validation
+    function checkEmail() {
+      const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+
+      if(!emailInput.value.match(emailPattern)){
+        return emailField.classList.add('invalid')
+      }
+
+      // Suppression de la classe invalide si l'e-mail correspond à emailPattern
+      emailField.classList.remove('invalid')
+    }
+
+    // Fonction d'appel sur la soumission du formulaire
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      checkEmail()
+    })
 
   }
     return self;
