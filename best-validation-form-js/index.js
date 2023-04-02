2e1dbfdee44d35ec8fbf350224e7b9d87c5ad495
Widdy LOUIS <louisws@outlook.fr>
2023-04-02 22:19:26 +0200

    Update index.js

--- best-validation-form-js/index.js
+++ best-validation-form-js/index.js
@@ -1,7 +1,20 @@
 const valForm = formSelect => {
   const formElt = document.querySelector(formSelect);
 
+  const validationOptions = [
+    {
+      attribute: 'required',
+      isValid: input => input.value.trim() !== '',
+      errorMessage: (input, label) => `${label.textContent} est requis`
+    }
+  ]
+
   const validSingleFormGroup = formGroup => {
+    const label = formGroup.querySelector('label');
+    const input = formGroup.querySelector('input, textarea');
+    const errorContainer = formGroup.querySelector('.error');
+    const errorIcon = formGroup.querySelector('.error-icon');
+    const successIcon = formGroup.querySelector('.success-icon');
     
   }
 
@@ -11,8 +24,11 @@ const valForm = formSelect => {
     validAllFormGroups(formElt)
   });
   
+  
   const validAllFormGroups = formToValid => {
+    // on recherche tous les container qui ont la classe '.formGroup'
     const formGroups = Array.from(formToValid.querySelectorAll('.formGroup'));
+     // on parse tous les formGroups trouvé et pour chacun on lance la fonction validSingleFormGroup
     formGroups.forEach( formGroup => {
       validSingleFormGroup(formGroup)
     })
