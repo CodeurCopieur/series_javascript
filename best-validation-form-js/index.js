6a5f03efa309d0942d7ab451ca4acab706c5e12c
Widdy LOUIS <louisws@outlook.fr>
2023-04-06 01:41:27 +0200

    Update index.js

--- best-validation-form-js/index.js
+++ best-validation-form-js/index.js
@@ -3,10 +3,20 @@ const valForm = formSelect => {
 
   const validationOptions = [
     {
+      attribute: 'minlength',
+      isValid: (input) => input.value && input.value.length >= parseInt(input.minLength, 10),
+      errorMessage: (input, label) => `Il faut au moins ${input.minLength} lettres pour votre ${label.textContent.toLowerCase()}`
+    },
+    {
+      attribute: 'custommaxlength',
+      isValid: (input) => input.value && input.value.length <= parseInt(input.getAttribute('custommaxlength'), 10),
+      errorMessage: (input, label) => `Il doit être inférieur à ${input.getAttribute('custommaxlength')} lettres pour votre ${label.textContent.toLowerCase()}`
+    },
+    {
       attribute: 'required',
       isValid: (input) => input.value.trim() !== '',
-      errorMessage: (input, label) => `${label.textContent} est requis`
-    }
+      errorMessage: (input, label) => `${label.textContent} est requis`,
+    },
   ]
 
   const validSingleFormGroup = formGroup => {
@@ -16,14 +26,29 @@ const valForm = formSelect => {
     const errorIcon = formGroup.querySelector('.error-icon');
     const successIcon = formGroup.querySelector('.success-icon');
     
+    let formGroupError = false;
     for(const option of validationOptions) {
       
       // l'input contient attribut required && qu'il n'est pas valid
       if (input.hasAttribute(option.attribute) && !option.isValid(input)) {
         // afficher le texte d'erreur dans l'élément html
-        errorContainer.textContent = option.errorMessage(input, label)
-        
+        errorContainer.textContent = option.errorMessage(input, label);
+        // ajouter ou supprimer des classes
+        input.classList.add('border-red-700');
+        input.classList.remove('border-green-700');
+        successIcon.classList.add('hidden');
+        errorIcon.classList.remove('hidden');
+        formGroupError = true;
       }
+    }
+
+    // si tout est bon
+    if (!formGroupError) {
+      errorContainer.textContent = '';
+      input.classList.add('border-green-700');
+      input.classList.remove('border-red-700');
+      successIcon.classList.remove('hidden');
+      errorIcon.classList.add('hidden');
     }
   }
 
