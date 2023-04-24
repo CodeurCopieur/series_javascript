88067d75294f5204f5b6ae2fe8e95be3b3bbba33
Widdy LOUIS <louisws@outlook.fr>
2023-04-24 23:19:23 +0200

    Create music.js

---
+++ music-player_js/#2/music.js
@@ -0,0 +1,20 @@
+let trackList = [
+  {
+    name: "A New Beginning",
+    path: "./music/01HotRemix.m4a",
+    img: "./img/01HotRemix.jpg",
+    singer: "01HotRemix.com",
+  },
+  {
+    name: "Hey",
+    path: "./music/01TacoTuesday.m4a",
+    img: "./img/01TacoTuesday.jpg",
+    singer: "01TacoTuesday.com",
+  },
+  {
+    name: "Little Idea",
+    path: "./music/01Wow.m4a",
+    img: "./img/01Wow.jpg",
+    singer: "01Wow.com",
+  }
+];
